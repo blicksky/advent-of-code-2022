544b71470d704f96f89aef9fbd4d4c070b93173e
Andrew Blick <andrew@blicksky.com>
2022-12-06 19:47:04 +0000

    cleanup

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,29 +1,37 @@
-export function fn(input: string, markerLength: number) {
-  let regexStr = "";
+export function findMarkerPositionWithRegExps(
+  input: string,
+  markerLength: number
+) {
+  let pattern = "";
 
   for (let i = 0; i <= markerLength - 2; ++i) {
     const lookAhead = markerLength - (i + 2);
     const captureGroup = i + 1;
-    regexStr += `(.)(?!(?:.{0,${lookAhead}})\\${captureGroup})`;
+    pattern += `(.)(?!(?:.{0,${lookAhead}})\\${captureGroup})`;
   }
 
-  const regex = new RegExp(regexStr);
-
-  const result = regex.exec(input)!;
-
-  return result.index + markerLength;
+  const results = new RegExp(pattern).exec(input)!;
+  return results.index + markerLength;
 }
 
-export function fnBetter(input: string, markerLength: number) {
-  for (let i = 0; i < input.length; ++i) {
-    const window = new Set<string>();
+export function findMarkerPositionWithSets(
+  input: string,
+  markerLength: number
+) {
+  for (let position = 0; position < input.length; ++position) {
+    const markerEndPosition = position + markerLength;
+    const markerCharacters = new Set<string>();
 
-    for (let w = i; w < i + markerLength; ++w) {
-      window.add(input[w]);
+    for (
+      let markerPosition = position;
+      markerPosition < markerEndPosition;
+      ++markerPosition
+    ) {
+      markerCharacters.add(input[markerPosition]);
     }
 
-    if (window.size === markerLength) {
-      return i + markerLength;
+    if (markerCharacters.size === markerLength) {
+      return markerEndPosition;
     }
   }
 }
