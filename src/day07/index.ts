15354acb43931c436aa9e2c1a411ed51e947a13c
Andrew Blick <andrew@blicksky.com>
2022-12-08 04:40:14 +0000

    simplify

--- src/day07/index.ts
+++ src/day07/index.ts
@@ -11,38 +11,26 @@ export function findSmallFiles(input: string): {
   const dirStack: Array<string> = [];
 
   commands.forEach(({ command, output }) => {
-    const currentDir = `/${dirStack.join("/")}`;
+    if (command === "cd /") {
+      dirStack.length = 0;
+    } else if (command.startsWith("cd")) {
+      const [, dir] = command.split(" ");
 
-    if (command) {
-      if (command === "cd /") {
-        dirStack.length = 0;
-      } else if (command.startsWith("cd")) {
-        const [, dir] = command.split(" ");
+      if (dir === "..") {
+        dirStack.pop();
+      } else {
+        dirStack.push(dir);
+      }
+    } else if (command === "ls") {
+      const dirSize = output.reduce((dirSize, line) => {
+        const size = parseInt(line, 10);
+        return dirSize + (isNaN(size) ? 0 : size);
+      }, 0);
 
-        if (dir === "..") {
-          dirStack.pop();
-        } else {
-          dirStack.push(dir);
-        }
-      } else if (command === "ls") {
-        const dirSize = output.reduce((dirSize, line) => {
-          const size = parseInt(line, 10) ?? 0;
-          return dirSize + (isNaN(size) ? 0 : size);
-        }, 0);
-
-        if (!sizesByDir.has(currentDir)) {
-          sizesByDir.set(currentDir, 0);
-        }
-
-        sizesByDir.set(currentDir, sizesByDir.get(currentDir)! + dirSize);
-
-        const dirStackCopy = [...dirStack];
-        while (dirStackCopy.length > 0) {
-          dirStackCopy.pop();
-          const curr = `/${dirStackCopy.join("/")}`;
-
-          sizesByDir.set(curr, sizesByDir.get(curr)! + dirSize);
-        }
+      for (let i = 0; i <= dirStack.length; ++i) {
+        const path = `/${dirStack.slice(0, i).join("/")}`;
+        const previousSize = sizesByDir.get(path) ?? 0;
+        sizesByDir.set(path, previousSize + dirSize);
       }
     }
   });
@@ -54,7 +42,8 @@ export function findSmallFiles(input: string): {
     0
   );
 
-  const unusedSpace = 70000000 - sizesByDir.get("/")!;
+  const usedSpace = sizesByDir.get("/") ?? 0;
+  const unusedSpace = 70000000 - usedSpace;
   const sizeToDelete = sizes
     .filter((size) => unusedSpace + size >= 30000000)
     .sort((a, b) => a - b)[0];
