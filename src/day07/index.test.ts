f60d2e9969d27c04c392e1230fd27e8978123b35
Andrew Blick <andrew@blicksky.com>
2022-12-08 04:11:14 +0000

    clean up

---
+++ src/day07/index.test.ts
@@ -0,0 +1,20 @@
+import { exampleInput, puzzleInput } from "./input";
+import { findSmallFiles } from "./index";
+
+describe("Day 5", () => {
+  describe("part 1 & 2", () => {
+    it("example", () => {
+      const { sumOfSmallSizes, sizeToDelete } = findSmallFiles(exampleInput);
+
+      expect(sumOfSmallSizes).toEqual(95437);
+      expect(sizeToDelete).toEqual(24933642);
+    });
+
+    it("puzzle", () => {
+      const { sumOfSmallSizes, sizeToDelete } = findSmallFiles(puzzleInput);
+
+      expect(sumOfSmallSizes).toEqual(1367870);
+      expect(sizeToDelete).toEqual(549173);
+    });
+  });
+});
