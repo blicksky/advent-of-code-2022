2ce03b24add072ee71480a3fe14d702d2b539d9c
Andrew Blick <andrew@blicksky.com>
2022-12-10 16:50:37 +0000

    day 10 part 2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -5,14 +5,36 @@ const instructions = puzzleInput.split("\n");
 let registerX = 1;
 let clock = 0;
 let signalStrengthSum = 0;
+let pixel = 0;
+let screen = "";
 
 const tick = () => {
   clock += 1;
+
+  if (
+    pixel + 1 === registerX ||
+    pixel === registerX ||
+    pixel - 1 === registerX
+  ) {
+    screen += "#";
+  } else {
+    screen += ".";
+  }
+
+  console.log(pixel, registerX);
+  console.log(screen);
 
   if ((clock + 20) % 40 === 0) {
     const signalStrength = clock * registerX;
     console.log({ clock, registerX, signalStrength });
     signalStrengthSum += signalStrength;
+  }
+
+  pixel += 1;
+
+  if (pixel === 40) {
+    screen += "\n";
+    pixel = 0;
   }
 };
 
