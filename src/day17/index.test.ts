47c83e2684e4478643268db3f952e7c93f13a63c
Andrew Blick <andrew@blicksky.com>
2022-12-17 13:04:12 +0000

    rock shape sequence

--- src/day17/index.test.ts
+++ src/day17/index.test.ts
@@ -1,4 +1,4 @@
-import { getGasJetSequence } from "./index";
+import { getGasJetSequence, getRockSequence, RockShape } from "./index";
 import { exampleInput, puzzleInput } from "./input";
 
 describe("Day 17", () => {
@@ -11,6 +11,24 @@ describe("Day 17", () => {
     }
 
     expect(gasJetDirections).toEqual([...exampleInput, ...exampleInput]);
+  });
+
+  it("produces rock shapes", () => {
+    const rockSequence = getRockSequence();
+
+    const rocks = [];
+    for (let i = 0; i < 16; i += 1) {
+      rocks.push(rockSequence.next().value);
+    }
+
+    expect(rocks[0]).toEqual(RockShape.HorizontalLine);
+    expect(rocks[1]).toEqual(RockShape.Plus);
+    expect(rocks[2]).toEqual(RockShape.Hook);
+    expect(rocks[3]).toEqual(RockShape.VerticalLine);
+    expect(rocks[4]).toEqual(RockShape.Square);
+    expect(rocks[5]).toEqual(RockShape.HorizontalLine);
+    expect(rocks[10]).toEqual(RockShape.HorizontalLine);
+    expect(rocks[15]).toEqual(RockShape.HorizontalLine);
   });
 
   describe.skip("part 1", () => {
--- src/day17/index.ts
+++ src/day17/index.ts
@@ -1,11 +1,66 @@
-type Left = "<";
-type Right = ">";
-type GasJetDirection = Left | Right;
-
-export function main() {}
+type GasJetLeft = "<";
+type GasJetRight = ">";
+type GasJetDirection = GasJetLeft | GasJetRight;
 
 export function* getGasJetSequence(pattern: string): Iterator<GasJetDirection> {
   for (let i = 0; true; i = (i + 1) % pattern.length) {
     yield pattern[i] as GasJetDirection;
   }
 }
+
+type RockShapeName =
+  | "HorizontalLine"
+  | "Plus"
+  | "Hook"
+  | "VerticalLine"
+  | "Square";
+
+type Coord = [number, number];
+type RockShape = Readonly<Array<Readonly<Coord>>>;
+
+export const RockShape: Readonly<Record<RockShapeName, RockShape>> = {
+  HorizontalLine: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+  ],
+  Plus: [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [3, 1],
+  ],
+  Hook: [
+    [0, 2],
+    [1, 2],
+    [3, 0],
+    [3, 1],
+    [3, 2],
+  ],
+  VerticalLine: [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+  ],
+  Square: [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1],
+  ],
+} as const;
+
+export function* getRockSequence(): Iterator<RockShape> {
+  while (true) {
+    yield RockShape.HorizontalLine;
+    yield RockShape.Plus;
+    yield RockShape.Hook;
+    yield RockShape.VerticalLine;
+    yield RockShape.Square;
+  }
+}
+
+export function main() {}
