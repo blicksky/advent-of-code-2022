dd34cedec6bab1dbff5f4c9c325a4aae09f62280
Andrew Blick <andrew@blicksky.com>
2022-12-22 02:25:18 +0000

    part 2

--- src/day21/index.test.ts
+++ src/day21/index.test.ts
@@ -1,4 +1,4 @@
-import { parseInput, main } from "./index";
+import { parseInput, main, main2 } from "./index";
 import { exampleInput, puzzleInput } from "./input";
 
 describe("Day 21", () => {
@@ -14,15 +14,19 @@ describe("Day 21", () => {
     });
   });
 
-  describe.skip("part 2", () => {
-    it("example input", () => {
+  describe("part 2", () => {
+    it("example input", async () => {
       const input = parseInput(exampleInput);
-      // expect(main()).toEqual(0);
+      // X = 301
+      expect(await main2(input)).toEqual("((4 + (2 * (X - 3))) / 4) = 150");
     });
 
-    it("puzzle input", () => {
+    it("example input", async () => {
       const input = parseInput(puzzleInput);
-      // expect(main()).toEqual(0);
+      // X = 3617613952378
+      expect(await main2(input)).toEqual(
+        "((((134300707002780 - (2 * (((294 + ((((((747 + (((((2 * ((((((((2 * (((((((((((45 + (((2 * (3 + (((2 * ((((10 * ((((3 * ((((((389 + (19 * ((((126 + ((12 * (18 + X)) - 541)) + 831) / 8) - 755))) / 2) - 642) + 31) / 2) + 784)) - 31) / 7) + 472)) + 936) / 2) - 875)) - 306) / 2))) - 975) / 11)) * 11) + 344) / 8) - 26) * 9) - 925) / 2) + 892) * 2) + 223)) - 931) / 5) - 69) * 2) + 12) / 2) + 549)) - 41) / 11) - 560) * 3)) / 2) + 150) / 4) - 542) * 20)) / 2) - 519))) * 2) + 257) / 5) = 26605796414957"
+      );
     });
   });
 });
--- src/day21/index.ts
+++ src/day21/index.ts
@@ -71,3 +71,59 @@ export async function main(jobs: MonkeyJob[]) {
 
   return await jobPromises.get("root")!();
 }
+
+// Idea for 2: don't do the math, but build an equation as a string with `humn` as the variable
+// TODO make this actually solve the equation it comes up with...
+export async function main2(jobs: MonkeyJob[]) {
+  const jobPromises = new Map<Name, () => Promise<string>>();
+
+  jobs.forEach((job) => {
+    if (isValueJob(job)) {
+      if (job.name === "humn") {
+        jobPromises.set(job.name, async () => "X");
+      } else {
+        jobPromises.set(job.name, async () => `${job.value}`);
+      }
+    } else {
+      jobPromises.set(job.name, async () => {
+        const [leftValue, rightValue] = await Promise.all([
+          jobPromises.get(job.leftOperand)!(),
+          jobPromises.get(job.rightOperand)!(),
+        ]);
+
+        if (job.name === "root") {
+          return `${leftValue} = ${rightValue}`;
+        } else {
+          const maybeLeftNumber = Number(leftValue);
+          const maybeRightNumber = Number(rightValue);
+
+          if (isNaN(maybeLeftNumber) || isNaN(maybeRightNumber)) {
+            switch (job.operation) {
+              case Operation.Add:
+                return `(${leftValue} + ${rightValue})`;
+              case Operation.Subtract:
+                return `(${leftValue} - ${rightValue})`;
+              case Operation.Multiply:
+                return `(${leftValue} * ${rightValue})`;
+              case Operation.Divide:
+                return `(${leftValue} / ${rightValue})`;
+            }
+          } else {
+            switch (job.operation) {
+              case Operation.Add:
+                return `${maybeLeftNumber + maybeRightNumber}`;
+              case Operation.Subtract:
+                return `${maybeLeftNumber - maybeRightNumber}`;
+              case Operation.Multiply:
+                return `${maybeLeftNumber * maybeRightNumber}`;
+              case Operation.Divide:
+                return `${maybeLeftNumber / maybeRightNumber}`;
+            }
+          }
+        }
+      });
+    }
+  });
+
+  return await jobPromises.get("root")!();
+}
